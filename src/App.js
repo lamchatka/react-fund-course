e8d164928d0e165ff079443dbf07bb027f9d5584
lamchatka <maxprolife@icloud.com>
2024-09-12 23:27:14 +0300

    created the posts sorting functional with useMemo; created the posts  searching functional with useMemo;

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/Select/MySelect";
+import MyInput from "./components/UI/Input/MyInput";
 
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "JavaScript", body: "JavaScript - язык программирования" },
-    { id: 2, title: "React", body: "React - библиотека" },
+    { id: 2, title: "React", body: " AReact - библиотека" },
     { id: 3, title: "HTML", body: "HTML - язык разметки" },
   ]);
 
@@ -18,7 +19,27 @@ function App() {
     { id: 3, title: "SQL", body: "SQL - Язык структурированных запросов" },
   ]);
 
+  // КЛЮЧ ПО КОТОРОМУ БУДЕМ СОРТИРОВАТЬ ПОСТЫ (ПО НАЗВАНИЮ, ПО ОПИСАНИЮ)
   const [selectedSort, setSelectedSort] = useState('');
+
+  // ЗНАЧЕНИЕ ПОЛЯ ИНПУТ ПО КОТОРОМУ БУДЕТ ПРОИСХОДИТЬ ФИЛЬТРАЦИЯ ПОСТОВ 
+  const [filteredValue, setFilteredValue] = useState('');
+
+  // СПИСОК ОТСОРТИРОВАННЫХ ПОСТОВ
+  const sortedPosts = useMemo(() => {
+    console.log("ФУНКЦИЯ СОРТИРУЕТ ПОСТЫ");
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+    }
+    return posts
+   
+  }, [selectedSort, posts]);
+
+  // СПИСОК ОТСОРТИРОВАННЫХ И НАЙДЕННЫХ ПО КЛЮЧЕВОМУ СЛОВУ filteredValue ПОСТЫ
+  const searchedAndSortedPosts = useMemo(() => {
+    console.log("ФУНКЦИЯ НАХОДИТ ПОСТЫ ПО СЛОВУ");
+    return sortedPosts.filter(p =>  p.title.toLowerCase().includes(filteredValue));
+  }, [filteredValue, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -30,13 +51,18 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   }
 
   return (
     <div className="App">
       <PostForm create={createPost}/>
       <hr style={{margin: "15px 0"}}/>
+      <MyInput
+        type="text"
+        placeholder="Поиск"
+        value={filteredValue}
+        onChange={e => setFilteredValue(e.target.value)}
+      />
       <MySelect
         value={selectedSort}
         onChange={sortPosts}
@@ -49,7 +75,7 @@ function App() {
       />
       {/* Условная отрисовка */}
       {posts.length 
-        ? <PostList posts={posts} title="Посты для JS" deletePost={deletePost}/> 
+        ? <PostList posts={searchedAndSortedPosts} title="Посты для JS" deletePost={deletePost}/> 
         : <h1 style={{textAlign: "center"}}>Посты не найдены</h1>
       }
      
