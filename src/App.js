e5729f521cfdf57b667e6b1bfb45d37aad7a9d46
lamchatka <maxprolife@icloud.com>
2024-09-17 13:45:07 +0300

    added block with paged number

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useSearchedAndSortedPosts } from "./components/hooks/usePosts";
 import PostService from "./API/PostService";
 import MyLoader from "./components/UI/Loader/MyLoader";
 import { useFetching } from "./components/hooks/useFetching";
-
+import { getPagesArray, getTotalPages } from "./utils/pages";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -18,16 +18,30 @@ function App() {
   const [filter, setFilter] = useState({sort: "", searchQuery: ""});
 
   const [visibleModal, setVisibleModal] = useState(false);
+
+  // ЛИМИТ ПОСТОВ НА ОДНОЙ СТРАНИЦЕ
+  const [limit, setLimit] = useState(10);
   
+  // СТРАНИЦА
+  const [page, setPage] = useState(1);
+
+  // КОЛ-ВО СТРАНИЦ
+  const [totalPages, setTotalPages] = useState(0);
+
+  // TODO: ИЗБАВИТЬСЯ ОТ ПЕРЕРЕНДЕРА С useMemo
+ const pagesArray = getPagesArray(totalPages);
+
   // СОСТОЯНИЕ ЗАГРУЗКИ ПОСТОВ 
   const [fetchPosts, isPostsLoading, postsError] = useFetching(async() => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getTotalPages(totalCount, limit));
+    
  })
 
   // КАСТОМНЫЙ ХУК useSearchedAndSortedPosts
   const searchedAndSortedPosts = useSearchedAndSortedPosts(posts, filter.sort, filter.searchQuery);
-
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -56,6 +70,15 @@ function App() {
       ? <MyLoader/>
       : <PostList posts={searchedAndSortedPosts} title="Посты для JavaScript" deletePost={deletePost}/>
       }
+      <div className="page__wrapper">
+        {pagesArray.map(p => 
+          <span 
+            key={p} 
+            className={page === p ? "page page__current" : "page"}
+            onClick={() => setPage(p)}
+          >{p}</span>
+        )}
+      </div>
     </div>
   );
 }
